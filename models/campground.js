f242cb7289661e22173d268c6a4955bff98bffac
Aseel Al Dallal <aseelaldallal@gmail.com>
2017-11-01 22:47:12 +0000

    Add rating

--- models/campground.js
+++ models/campground.js
@@ -1,12 +1,13 @@
 var mongoose = require("mongoose");
 
 var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    location: String, 
-    latlng: String,
-    description: String,
-    country: String,
+    name:         {type: String, required: true},
+    image:        {type: String, required: true},
+    location:     {type: String, required: true}, 
+    latlng:       {type: String, required: true},
+    description:  {type: String, required: true},
+    country:      {type: String, required: true},
+    avgRating:    {type: Number, default: 0, required: true },
     googlePlaceID: String,
     comments: [
         {
@@ -14,12 +15,19 @@ var campgroundSchema = new mongoose.Schema({
             ref: "Comment"
         }
     ],
+    ratings: [
+      {
+         type: mongoose.Schema.Types.ObjectId,
+         ref: "Rating"
+      }
+    ],
     author: {
         id: {
             type: mongoose.Schema.Types.ObjectId, 
-            ref: "User"
+            ref: "User",
+            required: true
         }, 
-        username: String
+        username: {type: String, required: true}
     }
 });
 
