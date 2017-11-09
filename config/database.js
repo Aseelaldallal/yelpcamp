72efd9a03f6c229e9ab62a7f8dcb4866f6bb097d
Aseel Al Dallal <aseelaldallal@gmail.com>
2017-11-09 19:55:13 +0000

    change db url

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@
 
 module.exports = {
 
-    'url' : 'mongodb://localhost/yelp_camp' || process.env.DB_URL
+    'url' : process.env.DB_URL
 
 };
 
-//process.env.DB_URL
+// 'mongodb://localhost/yelp_camp' 
