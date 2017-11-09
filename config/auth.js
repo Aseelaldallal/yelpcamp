919505fe26a0185fd0376e3179b065a95fc90d7e
Aseel Al Dallal <aseelaldallal@gmail.com>
2017-11-09 19:11:35 +0000

    change to heroku url

--- config/auth.js
+++ config/auth.js
@@ -5,13 +5,13 @@ module.exports = {
     'facebookAuth' : {
         'clientID'           : process.env.FACEBOOK_APP_ID, // your App ID
         'clientSecret'       : process.env.FACEBOOK_APP_SECRET, // your App Secret
-        'callbackURL'        : 'https://yelpcamp-asool.c9users.io/users/auth/facebook/callback',
+        'callbackURL'        : 'https://aseel-yelpcamp.herokuapp.com/users/auth/facebook/callback',
         'profileFields'      : ['id', 'emails', 'name'] 
     }, 
     'googleAuth' : {
         'clientID'         : process.env.GOOGLE_CLIENT_ID,
         'clientSecret'     : process.env.GOOGLE_CLIENT_SECRET,
-        'callbackURL'      : 'https://yelpcamp-asool.c9users.io/users/auth/google/callback'
+        'callbackURL'      : 'https://aseel-yelpcamp.herokuapp.com/users/auth/google/callback'
     }
     
 }
